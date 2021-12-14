afcc5121f057a3e99eb6627166e09c2a5e6fa2ae
DanielaSianaPabis <dani.s.pabis@gmail.com>
2021-12-14 18:22:01 -0300

    Inserindo RDE14 e JavaScript

---
+++ JavaScript/js/Ex05.js
@@ -0,0 +1,17 @@
+var n1, n2, divisao, multiplicacao, soma, subtracao
+
+n1= prompt("Digite o primeiro valor: ")
+
+n2= prompt("Digite o outro valor: ")
+
+soma= parseInt(n1) + parseInt(n2)
+alert("O resultado da soma entre estes números inteiros é: "+soma)
+
+subtracao= parseInt(n1) - parseInt(n2)
+alert("O resultado da subtração entre estes números inteiros é: "+subtracao)
+
+multiplicacao= parseInt(n1) * parseInt(n2)
+alert("O resultado da multiplicação entre estes números inteiros é: "+multiplicacao)
+
+divisao= parseInt(n1) / parseInt(n2)
+alert("O resultado da divisão entre estes números inteiros é: "+divisao)
---
+++ JavaScript/js/Ex06.js
@@ -0,0 +1,17 @@
+var n1, n2, divisao, multiplicacao, soma, subtracao
+
+n1= prompt("Digite o primeiro valor: ")
+
+n2= prompt("Digite o outro valor: ")
+
+soma= parseFloat(n1) + parseFloat(n2)
+alert("O resultado da soma entre estes números reais é: "+soma)
+
+subtracao= parseFloat(n1) - parseFloat(n2)
+alert("O resultado da subtração entre estes números reais é: "+subtracao)
+
+multiplicacao= parseFloat(n1) * parseFloat(n2)
+alert("O resultado da multiplicação entre estes números reais é: "+multiplicacao)
+
+divisao= parseFloat(n1) / parseFloat(n2)
+alert("O resultado da divisão entre estes números reais é: "+divisao)
