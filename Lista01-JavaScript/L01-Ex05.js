673430e5c48255252cdbf7ef19d506da49a5eb04
DanielaSianaPabis <dani.s.pabis@gmail.com>
2022-01-30 20:38:31 -0300

    Inserindo demais exercícios

--- Lista01-JavaScript/L01-Ex05.js
+++ Lista01-JavaScript/L01-Ex05.js
@@ -4,7 +4,14 @@ n=prompt("Digite o primeiro termo:")
 razao=prompt("Digite a razão:")
 quantT=prompt("Digite a quantidade de termos que deseja para inserir na progressão geométrica:")
 pg=n
+document.write("A Progressão Geométrica gerada é ( ")
+
 for(cont=1;cont<=quantT;cont++){
+    if(cont<quantT){
+        document.write(pg+" , ")
+    }
+    else{
+        document.write(pg+" ) ")
+    }
     pg=pg*razao
 }
-alert("A Progressão Geométrica gerada é: ")
