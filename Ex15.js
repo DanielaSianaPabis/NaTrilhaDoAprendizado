33efaeaafc91dfae6d12f94ff3c35fb42a495c21
DanielaSianaPabis <dani.s.pabis@gmail.com>
2022-02-02 11:44:06 -0300

    Inserindo Ex14 e Ex15 - Aula

---
+++ Ex15.js
@@ -0,0 +1,9 @@
+function somar(){
+    var n1, n2, resultado
+
+    n1=parseFloat(document.getElementById("n1").value)
+    n2=parseFloat(document.getElementById("n2").value)
+
+    res=n1+n2
+    document.getElementById("Resultado").innerHTML= `O resultado da soma é ${res}`
+}
