6f73f5ebc063442d9a74d9c81e19566315ca158f
DanielaSianaPabis <dani.s.pabis@gmail.com>
2022-02-14 13:51:38 -0300

    Atualizando Ex16 e inserindo Ex16 pt02

--- Ex16.js
+++ Ex16.js
@@ -1,7 +1,45 @@
 function Enviar(){
     var nome= document.getElementById("nome")
     var email = document.getElementById("email")
+    var datanasc = document.getElementById ("datanasc")
+    var comentario = document.getElementById("comentario")
+    var ok
+    
+    if(nome.value!="" && nome.value.IndexOf(" ")!=-1){
+        if(email.value.indexOf("@")!=-1){
+            if(datanasc.value!="" && datanasc.value<'2011'){
+                if(comentario.valuelength>49){
+                    alert(nome.value +" Agradecemos o seu contato!")
+                    ok=true
+                }
+                else{
+                    alert("O comentário deve possuir no mínimo 50 caracteres!")
+                    ok=false
+                }
+            }
+            else{
+                alert("Sua data de nascimento não pode estar além de 2010 e/ou verifique se prencheu este campo de forma correta!")
+                ok=false
+            }
+        }
+        else{
+            alert("Digite um e-mail válido!")
+            ok=false
+        }
+    }
+    else{
+        alert("Prencha o seu nome completo e de forma correta!")
+        ok=false
+    }
+    return ok
 
+}
+
+
+
+
+
+    /* FEITO EM SALA:
     if(nome.value!="" && nome.value.indexOf(" ")!=-1)
     {
         if(email.value.indexOf("@")!=-1)
@@ -21,5 +59,5 @@ function Enviar(){
         ok=false
     }
     return ok
-}
-/*ATIVIDADE: VALIDAR A DATA DE NASCIMENTO E OPCIONAL NÃO VALIDAR SE TIVER MENOS QUE 12 ANOS E FAZER UM CAMPO PRA VALIDAR NOME E SENHA*/
+
+ATIVIDADE: VALIDAR A DATA DE NASCIMENTO E OPCIONAL NÃO VALIDAR SE TIVER MENOS QUE 12 ANOS E FAZER UM CAMPO PRA VALIDAR NOME E SENHA*/
